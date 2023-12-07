f809f89508b8dc71baf4055f60494dbf6b78a5cf
Joalor64 <rhgarcon@gmail.com>
2023-12-07 10:24:15 -0500

    Update HUD.hx

--- source/ui/HUD.hx
+++ source/ui/HUD.hx
@@ -11,6 +11,9 @@ using flixel.util.FlxSpriteUtil;
 class HUD extends FlxTypedGroup<FlxSprite>
 {
     var hearts:Array<FlxSprite>;
+    
+    var coinsTxt:FlxText;
+    var pointsTxt:FlxText;
 
     public function new()
     {
